e5ba270a8cf8d19422b34cfe2e209593ee9b0bf8
Mthrun <m.thrun@gmx.net>
2018-04-23 09:44:26 +0200

    .

---
+++ R/PAMclustering.R
@@ -0,0 +1,17 @@
+PAMClustering=function(Data,ClusterNo,PlotIt=FALSE,...){
+  
+  #author: MT, 04/2018
+  requireNamespace('cluster')
+  pam=cluster::pam(x=Data,k=ClusterNo,...)
+  Cls=pam$clustering
+  if(!is.null(rownames(Data)))
+    names(Cls)=rownames(Data)
+  else
+    names(Cls)=1:nrow(Data)
+  
+  if(PlotIt){
+    requireNamespace('DataVisualizations')
+    DataVisualizations::plot3D(Data,Cls)
+  }
+  return(list(Cls=Cls,ObjectPAM=pam))
+}
