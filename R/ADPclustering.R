91cb3c967903e9be17d90c9aa799d03dcb355d55
Mthrun <m.thrun@gmx.net>
2020-12-14 09:43:58 +0100

    minor bugs

--- R/ADPclustering.R
+++ R/ADPclustering.R
@@ -25,6 +25,20 @@ ADPclustering=function(Data,ClusterNo=NULL,PlotIt=FALSE,...){
     )
   }
   
+  if (!requireNamespace('cluster', quietly = TRUE)) {
+    message(
+      'Subordinate clustering package (cluster) is missing although its imported in ADPclust. No computations are performed.
+            Please install the package which is defined in "Suggests".'
+    )
+    return(
+      list(
+        Cls = rep(1, nrow(Data)),
+        Object = "Subordinate clustering package (cluster) is missing  although its imported in ADPclust.
+                Please install the package which is defined in 'Suggests'."
+      )
+    )
+  }
+  
   if(is.null(ClusterNo))
 	adp=ADPclust::adpclust(Data,...)
   else
